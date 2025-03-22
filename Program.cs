ad8425648b7705f40adae2ae9e86882a0543baaf
Ra.khaihan <rakharaihan2244@gmail.com>
2025-03-22 17:15:21 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+
+namespace tpmodul6_103022300046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Rakha_Raihanurrahman");
+
+            //menampilkan video
+            video.PrintVideoDetails();
+
+            //menambahkan playcount
+            video.IncreasePlayCount(25);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Setelah ditambahkan playcount");
+            //menampilkan video setelah playcount ditambah
+            video.PrintVideoDetails();
+        }
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_103022300046
+{
+    //membuat class SayaTubeVideo
+    internal class SayaTubeVideo
+    {
+        //atribut
+        private int id;
+        private string title;
+        private int playCount;
+        
+        //konstruktor
+        public SayaTubeVideo(string title)
+        {
+            //generate id secara random sebanyak 5 digit
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+
+            //inisialisasi atribut
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        //method IncreasePlayCount
+        public void IncreasePlayCount(int playCount)
+        {
+            this.playCount += playCount;
+        }
+
+
+        //method PrintVideoDetail
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID Video: " + id);
+            Console.WriteLine("Judul Video: " + title);
+            Console.WriteLine("Jumlah Play: " + playCount);
+        }
+    }
+}
